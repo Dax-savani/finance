0eb59e3d74cbab330c5ac76b8aba88325bbc1a51
Savani <163087650+Dax-savani@users.noreply.github.com>
2026-02-14 16:33:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 import NcdQueries from "./pages/NCDQueries/NcdQueries";
 import Policy from "./pages/policy/Policy";
-//tttt
+//ttt
 function App() {
   useEffect(() => {
     Aos.init({
